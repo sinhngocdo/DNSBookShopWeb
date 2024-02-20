886f0bd415b1544f39cce852e14cb322e2b485a5
sinhngocdo <sinhngocdo@gmail.com>
2024-02-20 15:50:49 +0700

    Add project files.

---
+++ DNSBookShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DNSBookShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

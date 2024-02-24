3d84c293bc6d798f8d51e7498b8bb4839391a876
sinhngocdo <sinhngocdo@gmail.com>
2024-02-24 16:31:22 +0700

    Shoppping-cart-SummayUI

--- DNSBookShopWeb.Models/ShoppingCart.cs
+++ DNSBookShopWeb.Models/ShoppingCart.cs
@@ -25,5 +25,8 @@ namespace DNSBookShopWeb.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
     }
 }

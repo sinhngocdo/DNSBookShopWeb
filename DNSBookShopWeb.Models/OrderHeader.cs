9fe504cc3459b7e90b16a6c19600aeb65d4b3b4f
sinhngocdo <sinhngocdo@gmail.com>
2024-02-27 16:05:22 +0700

    OrderConfirmation-AddHelperMethodInOrderHeaderRepository

--- DNSBookShopWeb.Models/OrderHeader.cs
+++ DNSBookShopWeb.Models/OrderHeader.cs
@@ -30,7 +30,8 @@ namespace DNSBookShopWeb.Models
 
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate {  get; set; }
-        
+
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]

12bdd37ccb55fc2b1e0ee9bdca29dee85f75b9f9
sinhngocdo <sinhngocdo@gmail.com>
2024-02-28 18:37:03 +0700

    OrderController and OrderVM

---
+++ DNSBookShopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using DNSBookShopWeb.DataAccess.Repository.IRepository;
+using DNSBookShopWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNSBookShopWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+        
+        #endregion
+    }
+}

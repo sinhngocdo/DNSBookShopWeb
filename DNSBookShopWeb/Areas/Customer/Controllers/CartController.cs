1e776a1ff5f5427110a66f8bc084bd176ae57d41
sinhngocdo <sinhngocdo@gmail.com>
2024-02-28 15:10:55 +0700

    OrderConfirmation-ConfirmPaymentStripe

--- DNSBookShopWeb/Areas/Customer/Controllers/CartController.cs
+++ DNSBookShopWeb/Areas/Customer/Controllers/CartController.cs
@@ -125,7 +125,7 @@ namespace DNSBookShopWeb.Areas.Customer.Controllers
             {
                 //it is a regular customer account and we need to capture payment
                 //stripe logic
-                var domain = "https://localhost:7169/";
+                var domain = "https://localhost:7270/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
@@ -168,6 +168,30 @@ namespace DNSBookShopWeb.Areas.Customer.Controllers
 
         public IActionResult OrderConfirmation(int id)
         {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                //this is an order by customer
+
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+
+
+            }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
+                .GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+
+            _unitOfWork.ShoppingCart.DeleteRange(shoppingCarts);
+            _unitOfWork.Save();
+
             return View(id);
         }
 

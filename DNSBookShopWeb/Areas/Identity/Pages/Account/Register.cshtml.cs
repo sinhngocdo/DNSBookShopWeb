bdc1890e82065b702ef2c125a11a0ae490f5c624
sinhngocdo <sinhngocdo@gmail.com>
2024-03-07 23:12:38 +0700

    Create Employee and Admin accout

--- DNSBookShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DNSBookShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -218,7 +218,14 @@ namespace DNSBookShopWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New user Created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }

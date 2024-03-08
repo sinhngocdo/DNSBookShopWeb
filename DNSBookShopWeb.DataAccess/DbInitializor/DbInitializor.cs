fd782a85362049f9afc096d790455dc4f49bba8a
sinhngocdo <sinhngocdo@gmail.com>
2024-03-08 11:01:35 +0700

    DbInitializor

---
+++ DNSBookShopWeb.DataAccess/DbInitializor/DbInitializor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSBookShopWeb.DataAccess.DbInitializor
+{
+    public class DbInitializor : IDbInitializor
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
